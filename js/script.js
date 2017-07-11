75632d980263a97bf3eea3e92bb35eaee1b7338c
m9dfukc <andreas@invertednothing.com>
2017-07-11 18:04:41 +0200

    with popup

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $(function() {
-  $('.l1 .w1').fitText(0.1);
+  var win;
+  var opened = false;
+  $('#c1 .l1 .w1').fitText(0.13);
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= $(window).height() && !opened) {
+      win = window.open("file:///Users/m9dfukc/Desktop/mozilla/_sketch_/second.html", "_blank", "width=500, height=600, left=1000");
+      opened = true;
+    }
+    if ($(window).scrollTop() < $(window).height()) {
+      try {
+        win.close();
+        opened = false;
+      } catch(e) {
+
+      }
+    }
+  });
 });
