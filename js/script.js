d6758d685de961be62cc7f8a82d0c118f75081c4
m9dfukc <andreas@invertednothing.com>
2017-07-11 19:11:19 +0200

    fix absolute file path

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(function() {
   $('#c1 .l1 .w1').fitText(0.15);
   $(window).resize(function() {
     if ($(window).width() >= screen.width * 0.85 && !opened) {
-      win = window.open("file:///Users/m9dfukc/Desktop/mozilla/_sketch_/second.html", "_blank", "width=170, height=240, left=1400, top=90");
+      win = window.open("second.html", "_blank", "width=170, height=240, left=1400, top=90");
       opened = true;
     }
     if ($(window).width() < screen.width * 0.75) {
