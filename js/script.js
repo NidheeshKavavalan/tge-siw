a7002a99bd70a72cef6036b3b3df60cf7859c55f
m9dfukc <andreas@invertednothing.com>
2017-07-17 09:45:43 +0200

    cleanup code

--- js/script.js
+++ js/script.js
@@ -1,50 +1,27 @@
-var tStart = 500 
-  , tEnd = 700   
-  , cStart = [255, 255, 255] 
-  , cEnd = [0, 0, 0]   
+var tStart = 500
+  , tEnd = 700
+  , cStart = [255, 255, 255]
+  , cEnd = [0, 0, 0]
   , cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[2] - cStart[2]];
 
-
 $(function() {
-  var win;
-  var opened = false;
+  var win1, win2;
+  var opened1, opened2 = false;
   $('#c1 .l1 .w1').fitText(0.15);
   $(window).resize(function () {
-    if ($(window).width() >= screen.width * 0.76 && !opened) {
-      win = window.open("second.html", "_blank", "width=370, height=200, left=1500, top=25");
-      opened = true;
+    if ($(window).width() >= screen.width * 0.64 && !opened1) {
+      win2 = window.open("third.html", "_blank", "width=300, height=225, left=1000, top=700");
+      opened1 = true;
     }
-    if ($(window).width() < screen.width * 0.66) {
-      try {
-        win.close();
-        opened = false;
-      } catch(e) {}
+    if ($(window).width() >= screen.width * 0.76 && opened2) {
+      win1 = window.open("second.html", "_blank", "width=370, height=200, left=1500, top=25");
+      opened2 = true;
     }
   });
-   });
-
- $(function() {
-  var win;
-  var opened = false;
-  $(window).resize(function () {
-    if ($(window).width() >= screen.width * 0.64 && !opened) {
-      win = window.open("third.html", "_blank", "width=300, height=225, left=1000, top=700");
-      opened = true;
-    }
-    if ($(window).width() < screen.width * 0.54) {
-      try {
-        win.close();
-        opened = false;
-      } catch(e) {}
-    }
-  });
-
   $(document).scroll(function() {
-        var p = ($(this).scrollTop() - tStart) / (tEnd - tStart); 
-        p = Math.min(1, Math.max(0, p)); 
-        var cBg = [Math.round(cStart[0] + cDiff[0] * p), Math.round(cStart[1] + cDiff[1] * p), Math.round(cStart[2] + cDiff[2] * p)];
-        $("body").css('background-color', 'rgb(' + cBg.join(',') +')');
-    });
-  
- 
+    var p = ($(this).scrollTop() - tStart) / (tEnd - tStart);
+    p = Math.min(1, Math.max(0, p));
+    var cBg = [Math.round(cStart[0] + cDiff[0] * p), Math.round(cStart[1] + cDiff[1] * p), Math.round(cStart[2] + cDiff[2] * p)];
+    $("body").css('background-color', 'rgb(' + cBg.join(',') +')');
+  });
 });
--- js/script02.js
+++ js/script02.js
@@ -1,23 +1,21 @@
 $(function() {
   var win;
+  var left = 1100;
+  var top = 50;
+  var interval = 50;
   var opened = false;
   $('#c3 .l1 .w1, .w2, .w3').fitText(0.15);
   $(window).resize(function() {
     if ($(window).width() >= screen.width * 0.48 && !opened) {
-      win = window.open("fourth.html", "_blank", "width=400, height=400, left=1100, top=50");
+      win = window.open("fourth.html", "_blank", "width=400, height=445, left="+left+", top="+top);
       opened = true;
-    }
-    if ($(window).width() < screen.width * 0.38) {
-      try {
-        win.close();
-        opened = false;
-      } catch(e) {}
+      jitter();
     }
   });
+  function jitter() {
+    var offsetTop = Math.floor(Math.random() * 10) + top;
+    var offsetLeft = Math.floor(Math.random() * 10) + left;
+    win.moveTo(offsetLeft, offsetTop);
+    setTimeout(jitter, interval);
+  }
 });
-
-
-
-
-
-
